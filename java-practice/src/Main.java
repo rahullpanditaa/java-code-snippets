d05671e6f67f5ffd50a2ae85f7d8943e7f82ea35
Rahul Pandita <rpandita1999@outlook.com>
2025-01-17 15:08:55 +0530

    common issue in accepting string input after int or double

--- java-practice/src/Main.java
+++ java-practice/src/Main.java
@@ -5,30 +5,20 @@ public class Main {
     // the main method is needed for the program to run
     public static void main(String[] args) {
 
+        // opens the input stream I guess?
+        // otherwise console is read only
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
+        System.out.print("Enter today's date: ");
+        int date = scanner.nextInt();
+        // get rid of newline character
+        scanner.nextLine();
 
-        // next line method of scanner object reads
-        // a string of characters
-        String name = scanner.nextLine();
+        System.out.print("Enter today's day: ");
+        String day = scanner.nextLine();
 
-        System.out.println("Hello " + name);
-
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
-        System.out.println("Your age is " + age + " years.");
-
-        System.out.print("What is your weight (in kgs)? : ");
-        double weight = scanner.nextDouble();
-        System.out.println(weight + " kgs.");
-
-        if (weight > 65.0) {
-            System.out.println("Go to the gym.");
-        } else {
-            System.out.println("Kuposhan hai aapko.");
-        }
-
+        System.out.println("Today's date: " + date);
+        System.out.println("Today's day: " + day);
 
         scanner.close();
 
