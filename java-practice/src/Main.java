5f036d22172444b547677a6a635e0a49014cd471
Rahul Pandita <rpandita1999@outlook.com>
2025-01-17 14:27:14 +0530

    main method - needed for the program to run; print to the console

---
+++ java-practice/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    // the main method is needed for the program to run
+    public static void main(String[] args) {
+
+        System.out.println("I like pizza");
+        System.out.print("It's really good\n");
+        System.out.print("Buy me some pizza");
+
+    }
+}
