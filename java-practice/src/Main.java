9be2abbd8fe4bdc8718f6e7fb9a62b9e6c254e98
Rahul Pandita <rpandita1999@outlook.com>
2025-01-17 14:38:22 +0530

    a few primitive data types; int, double, char, boolean

--- java-practice/src/Main.java
+++ java-practice/src/Main.java
@@ -3,9 +3,30 @@ public class Main {
     // the main method is needed for the program to run
     public static void main(String[] args) {
 
-        System.out.println("I like pizza");
-        System.out.print("It's really good\n");
-        System.out.print("Buy me some pizza");
+        // 2 steps to creating a variable:
+        // 1. declaration
+        // 2. assignment
+
+        int age = 33;
+        int year = 2025;
+        int quantity = 8;
+
+        double price = 200.99;
+        double cgpa = 5.7;
+        double weight = 90;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isHandsome = true;
+        boolean isSmart = false;
+
+//        System.out.println(price);
+//        System.out.println(weight);
+
+//        System.out.println(age);
+//        System.out.println("The year is " + year);
 
     }
 }
